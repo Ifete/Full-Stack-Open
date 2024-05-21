e2670f0fb7f01816c724530566afe9f7837816d9
Irene <ireneterra20@gmail.com>
2024-05-21 11:36:28 +0200

    2.15*: La Agenda Telefónica paso 10

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,102 +1,111 @@
 //import { useState } from 'react'
-import { useState, useEffect } from 'react'
-import axios from 'axios'
-import Name from './components/Name'
-import Filter from './components/Filter'
-import PersonForm from './components/PersonForm'
-import personService from './services/persons'
-
+import { useState, useEffect } from "react";
+import axios from "axios";
+import Name from "./components/Name";
+import Filter from "./components/Filter";
+import PersonForm from "./components/PersonForm";
+import personService from "./services/persons";
 
 const App = () => {
-  const [persons, setPersons] = useState([])
+  const [persons, setPersons] = useState([]);
 
-  const [newName, setNewName] = useState('')
-  const [newPhone, setNewPhone] = useState('')
-  const [newFilter, setNewFilter] = useState('')
-
+  const [newName, setNewName] = useState("");
+  const [newPhone, setNewPhone] = useState("");
+  const [newFilter, setNewFilter] = useState("");
 
   useEffect(() => {
-    personService
-      .getAll()
-      .then(response => {
-       //console.log('promise fulfilled')
-        setPersons(response.data)
-      })
-  }, [])
-    console.log(persons.length)
+    personService.getAll().then((response) => {
+      //console.log('promise fulfilled')
+      setPersons(response.data);
+    });
+  }, []);
+  console.log(persons.length);
 
   const addData = (event) => {
-    event.preventDefault()
-    let existeNombre = false
+    event.preventDefault();
+    let existeNombre = false;
     for (let i = 0; i < persons.length; i++) {
       if (persons[i].name === newName) {
-        existeNombre = true
+        existeNombre = true;
       }
     }
     if (existeNombre) {
-      window.alert(`${newName} is already added to phonebook`)
+      let conf = window.confirm(
+        newName +
+          "is already added to phonebook, replace the old number with a new one?"
+      );
+      if (conf) {
+        const person = persons.find((n) => n.name === newName);
+        let id = person.id
+        console.log(id)
+        const changedPersons = { ...person, number: newPhone };
+        personService
+        .update(id, changedPersons)
+        .then(returnedPerson => {
+          setPersons(persons.map(person => person.id !== id ? person : returnedPerson))
+          setNewName("");
+          setNewPhone("");
+        })
+      }
     } else {
       const personObject = {
         name: newName,
-        number: newPhone
-      }
-      personService
-        .create(personObject)
-        .then(response => {
-          console.log(response)
-          setPersons(persons.concat(response.data))
-          setNewName('')
-          setNewPhone('')
-        })
-
+        number: newPhone,
+      };
+      personService.create(personObject).then((response) => {
+        console.log(response);
+        setPersons(persons.concat(response.data));
+        setNewName("");
+        setNewPhone("");
+      });
     }
-  }
+  };
 
   const handleNameChange = (event) => {
     //console.log(event.target.value)
-    setNewName(event.target.value)
-  }
+    setNewName(event.target.value);
+  };
   const handlePhoneChange = (event) => {
     //console.log(event.target.value)
-    setNewPhone(event.target.value)
-  }
+    setNewPhone(event.target.value);
+  };
 
   const handleFilterChange = (event) => {
-    console.log(event.target.value)
-    setNewFilter(event.target.value)
+    console.log(event.target.value);
+    setNewFilter(event.target.value);
     if (event.target.value.trim().length) {
       for (let i = 0; i < persons.length; i++) {
-        if (!persons[i].name.toUpperCase().includes(event.target.value.toUpperCase())) {
+        if (
+          !persons[i].name
+            .toUpperCase()
+            .includes(event.target.value.toUpperCase())
+        ) {
           persons.splice(i, 1);
         }
       }
     }
-  }
+  };
 
-  const removePerson = id => {
-    const person = persons.find(p => p.id === id)
+  const removePerson = (id) => {
+    const person = persons.find((p) => p.id === id);
     let name = person.name;
-    let conf = window.confirm('Delete ' + name)
-    console.log(person.id)
+    let conf = window.confirm("Delete " + name);
+    console.log(person.id);
     if (conf) {
       //console.log("Confirm:", person.id)
       personService
         .remove(id)
-        .then(returnedPerson => {
-          let removedPers = persons.filter(el => el.name !== returnedPerson.name); 
-          console.log(removedPers)
-          setPersons(removedPers)          
+        .then((returnedPerson) => {
+          let removedPers = persons.filter(
+            (el) => el.name !== returnedPerson.name
+          );
+          setPersons(removedPers);
         })
-        .catch(error => {
-          alert(
-            `the person '${person.name}' was already deleted from server`
-          )
-        })
-    }else{
-      console.log('Cancel')
+        .catch((error) => {
+          alert(`the person '${person.name}' was already deleted from server`);
+        });
     }
-  }
-
+  };
 
   return (
     <div>
@@ -114,15 +123,15 @@ const App = () => {
       />
 
       <h3>Numbers</h3>
-      {persons.map(person =>
-
-          <Name
+      {persons.map((person) => (
+        <Name
           key={person.id}
           name={person}
-          removePerson={() => removePerson(person.id)} />
-    )}
+          removePerson={() => removePerson(person.id)}
+        />
+      ))}
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- part2/phonebook/src/services/persons.js
+++ part2/phonebook/src/services/persons.js
@@ -12,4 +12,9 @@ const create = newObject => {
     const request = axios.delete(`${baseUrl}/${id}`)
     return request.then(response => response.data)
   }
-export default { getAll, create, remove }
+
+  const update = (id, newObject) => {
+    const request = axios.put(`${baseUrl}/${id}`, newObject)
+    return request.then(response => response.data)
+  }
+export default { getAll, create, remove, update }
