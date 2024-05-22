f705f6b6c711d75680c16169d642672adbe3df00
Ifete <ireneterra20@gmail.com>
2024-05-22 11:07:14 +0200

    2.18* Datos de países, paso 1

--- part2/countries/src/App.jsx
+++ part2/countries/src/App.jsx
@@ -1,27 +1,61 @@
 import { useState, useEffect } from 'react'
 import axios from "axios";
+import Notification from "./components/Notification";
+
+
 
 function App() {
   const [name, setName] = useState('')
-  const [countries, setCountry] = useState({})
+  const [countries, setCountry] = useState([])
+  const [oneCountry, setOneCountry] = useState([])
   const [countSearch, setCountSearch] = useState(null)
+
+  const [message, setMessage] = useState(null)
 
   useEffect(() => {
     console.log('effect run, countSearch is now', countSearch)
 
-    // omitir si la moneda no está definida
+    // omitir si el pais de búsqeuda no está definido
     if (countSearch) {
       console.log('fetching countries...')
       axios
-        .get(`https://studies.cs.helsinki.fi/restcountries/api/name/${countSearch}`)
+        .get(`https://studies.cs.helsinki.fi/restcountries/api/all`)
         .then(response => {
-          setCountry(response.data.name.common)
+          let paises = []
+          //console.log(response.data.length)
+          for (let i = 0; i < response.data.length; i++) {
+            //console.log(response.data[i].name.common)
+            if (
+              response.data[i].name.common
+                .toUpperCase()
+                .includes(countSearch.toUpperCase())
+            ){
+              //console.log('includes: ', response.data[i].name.common)
+              paises.push(response.data[i])
+            }
+          }
+          if(paises.length==1){
+            setOneCountry(paises)
+            setCountry([])
+          }else if(paises.length>10){
+            setCountry([])
+            setOneCountry([])
+            setMessage(
+              `Too many matches, specify another filter'`
+            )
+            setTimeout(() => {
+              setMessage(null)
+            }, 4000)
+          }else{
+            setCountry(paises)
+            setOneCountry([])
+          }
         })
     }
   }, [countSearch])
 
   const handleChange = (event) => {
-    console.log('name', event.target.value)
+    //console.log('name', event.target.value)
     setName(event.target.value)
   }
 
@@ -30,15 +64,42 @@ function App() {
     setCountSearch(name)
   }
 
+  //console.log(countries.length)
+  //console.log('oneCountry: ',oneCountry)
+
   return (
     <div>
     <form onSubmit={onSearch}>
       Country: <input value={name} onChange={handleChange} />
       <button type="submit">find country</button>
     </form>
+    <Notification message={message} />
     <pre>
-      {JSON.stringify(countries, null, 2)}
+      {countries.map((country) => (
+        <p key={country.tld.cca3}>{country.name.common}</p>
+      ))}
     </pre>
+    <pre>
+      {oneCountry.map((country) => (
+        <div key={country.cca3}>
+          <h1>{country.name.common}</h1>
+          <p>capital {country.capital}</p>
+          <p>area {country.area}</p>
+          <h2>lenguages:</h2>
+          {
+          <ul>
+            {Object.values(country.languages).map(lang =>
+              <li key={lang}>
+                <span style={{fontWeight:'bold'}}>{lang}</span>
+              </li>
+            )}
+          </ul>}
+          <img style={{width : '100px', height : '80px'}} src={country.flags.svg}></img>
+        </div>
+      ))}
+    </pre>
+
+
   </div>
   )
 }
